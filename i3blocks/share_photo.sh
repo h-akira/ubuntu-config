f5993f491bdf99a7953068af03f22db20ba5cc23
h-akira <79441679+h-akira@users.noreply.github.com>
2023-02-25 14:19:50 +0900

    fix

--- i3blocks/share_photo.sh
+++ i3blocks/share_photo.sh
@@ -3,4 +3,4 @@
 # Created:      2023-02-25 13:32:43
 set -eu
 
-echo "Photo: `cat /home/hakira/.local/opt/Share_Photo/secret/state.txt`"
+echo "Photo: `cat ${HOME}/.local/opt/Share_Photo/secret/state.txt`"
