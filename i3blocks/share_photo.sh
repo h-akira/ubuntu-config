ec33aa7c79bfd1766792dfda1b190930dfeb5ec4
h-akira <79441679+h-akira@users.noreply.github.com>
2023-03-14 14:54:37 +0900

    add color

--- i3blocks/share_photo.sh
+++ i3blocks/share_photo.sh
@@ -3,4 +3,13 @@
 # Created:      2023-02-25 13:32:43
 set -eu
 
-echo "Photo: `cat ${HOME}/.local/opt/Share_Photo/secret/state.txt`"
+# echo "Photo: `cat ${HOME}/.local/opt/Share_Photo/secret/state.txt`"
+STATE="`cat ${HOME}/.local/opt/Share_Photo/secret/state.txt`"
+if [ "${STATE}" != "Updated" ]; then
+  COLOR_DOWN=${COLOR_DOWN:-#FF0000}
+  echo "Photo: ${STATE}"
+  echo "Photo: ${STATE}"
+  echo ${COLOR_DOWN}
+else
+  echo "Photo: ${STATE}"
+fi
