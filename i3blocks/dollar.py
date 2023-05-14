75c54adc3faa5a81942881f60a99a56ae4cb9905
h-akira <79441679+h-akira@users.noreply.github.com>
2023-05-15 02:39:14 +0900

    update dollar

--- i3blocks/dollar.py
+++ i3blocks/dollar.py
@@ -4,10 +4,11 @@
 # Created: 2023-05-15 01:42:51
 
 # Import
+import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def main():
+def get_rate():
   URL = "https://www.click-sec.com/corp/guide/fxneo/rate/"
   CHROMEDRIVER = "/usr/bin/chromedriver"
   options = Options()
@@ -16,7 +17,27 @@ def main():
   driver.get(URL)
   dollar_yen_buy = driver.find_element_by_id("fxneorate_bid_0").text
   driver.quit()
-  print(f"USD/JPY={dollar_yen_buy}")
+  return dollar_yen_buy
+
+def market_open():
+  DIFF_JST_FROM_UTC = 9
+  now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+  num = now.weekday()
+  if 1 <= num <= 4:
+    return True
+  elif num == 0 and now.hour >= 7:  
+    return True
+  elif num == 5 and now.hour < 7:
+    return True
+  else:
+    return False
+
+def main():
+  if market_open():
+    dollar_yen_buy = get_rate()
+    print(f"USD/JPY={dollar_yen_buy}")
+  else:
+    print("Market is not open")
 
 if __name__ == '__main__':
   main()
